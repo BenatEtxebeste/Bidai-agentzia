77fafd5143ec795292c552481206b73e62259bb8
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-08 13:29:16 +0100

    clase visor terminada

--- src/Visor.java
+++ src/Visor.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
 
 public class Visor {
-
+	public static void mostrarCliente(Cliente cliente) {
+		System.out.println(cliente);
+	}
+	
+	public static void mostrarClientes(ArrayList<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			System.out.println(cliente);
+		}
+	}
+	
+	public static void mostrarHabitacion(Habitacion habitacion) {
+		System.out.println(habitacion);
+	}
+	
+	public static void mostrarHabitaciones(ArrayList<Habitacion> habitaciones) {
+		for (Habitacion habitacion : habitaciones) {
+			System.out.println(habitacion);
+		}
+	}
+	
+	public static void mostrarHotel(Hotel hotel) {
+		System.out.println(hotel);
+	}
+	
+	public static void mostrarHoteles(ArrayList<Hotel> hoteles) {
+		for (Hotel hotel : hoteles) {
+			System.out.println(hotel);
+		}
+	}
+	
+	public static void mostrarReserva(Reserva reserva) {
+		System.out.println(reserva);
+	}
+	
+	public static void mostrarReservas(ArrayList<Reserva> reservas) {
+		for (Reserva reserva : reservas) {
+			System.out.println(reserva);
+		}
+	}
 }
