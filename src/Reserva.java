7cca8b435337fcd10e8711f88e8188b0b72f185e
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-08 13:56:03 +0100

    clase reserva completada

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,10 +1,11 @@
+import java.sql.Date;
 
 public class Reserva {
 	private int id;
-	private int id_hotel;
-	private String numero;
-	private String descripcion;
-	private double precio;
+	private int id_habitacion;
+	private String dni;
+	private Date desde;
+	private Date hasta;
 	
 	public int getId() {
 		return id;
@@ -12,34 +13,34 @@ public class Reserva {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getId_hotel() {
-		return id_hotel;
+	public int getId_habitacion() {
+		return id_habitacion;
 	}
-	public void setId_hotel(int id_hotel) {
-		this.id_hotel = id_hotel;
+	public void setId_habitacion(int id_habitacion) {
+		this.id_habitacion = id_habitacion;
 	}
-	public String getNumero() {
-		return numero;
+	public String getDni() {
+		return dni;
 	}
-	public void setNumero(String numero) {
-		this.numero = numero;
+	public void setDni(String dni) {
+		this.dni = dni;
 	}
-	public String getDescripcion() {
-		return descripcion;
+	public Date getDesde() {
+		return desde;
 	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
+	public void setDesde(Date desde) {
+		this.desde = desde;
 	}
-	public double getPrecio() {
-		return precio;
+	public Date getHasta() {
+		return hasta;
 	}
-	public void setPrecio(double precio) {
-		this.precio = precio;
+	public void setHasta(Date hasta) {
+		this.hasta = hasta;
 	}
 	
 	@Override
 	public String toString() {
-		return "GestorReserva [id=" + id + ", id_hotel=" + id_hotel + ", numero=" + numero + ", descripcion="
-				+ descripcion + ", precio=" + precio + "]";
+		return "Reserva [id=" + id + ", id_habitacion=" + id_habitacion + ", dni=" + dni + ", desde=" + desde
+				+ ", hasta=" + hasta + "]";
 	}
 }
