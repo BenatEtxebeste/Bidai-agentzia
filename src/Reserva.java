374a36184d7c79b8882f11930f480fb130cbbdd6
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-08 13:45:04 +0100

    clase reserva completa

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,4 +1,45 @@
 
 public class Reserva {
-
+	private int id;
+	private int id_hotel;
+	private String numero;
+	private String descripcion;
+	private double precio;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId_hotel() {
+		return id_hotel;
+	}
+	public void setId_hotel(int id_hotel) {
+		this.id_hotel = id_hotel;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	@Override
+	public String toString() {
+		return "GestorReserva [id=" + id + ", id_hotel=" + id_hotel + ", numero=" + numero + ", descripcion="
+				+ descripcion + ", precio=" + precio + "]";
+	}
 }
