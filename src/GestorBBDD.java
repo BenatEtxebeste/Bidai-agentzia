269dfe458d64d318625ce5f306e1285da68e41b5
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-13 12:58:35 +0100

    getCliente created

--- src/GestorBBDD.java
+++ src/GestorBBDD.java
@@ -1,4 +1,39 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class GestorBBDD {
+public class GestorBBDD extends Conector{
+	PreparedStatement pst;
 
+	public void insertarCliente(Cliente cliente) throws SQLException {
+		pst = super.con.prepareStatement("INSERT INTO clientes VALUES(?, ?, ?, ?, ?)");
+		pst.setString(1, cliente.getDni());
+		pst.setString(2, cliente.getNombre());
+		pst.setString(3, cliente.getApellidos());
+		pst.setString(4, cliente.getDireccion());
+		pst.setString(5, cliente.getLocalidad());
+		
+		pst.execute();
+	}
+	
+	public void eliminarCliente(String dni) throws SQLException{
+		pst = super.con.prepareStatement("DELETE FROM clientes WHERE dni= ?");
+		pst.setString(1, dni);
+		
+		pst.execute();
+	}
+	
+	public Cliente getCliente(String dni) throws SQLException {
+		String sentenciaSelect = "SELECT FROM clientes WHERE dni = ?";
+		pst.setString(1, dni);
+		Cliente cliente = new Cliente();
+		ResultSet resultado = pst.executeQuery(sentenciaSelect);
+		cliente.setDni(resultado.getString("dni"));
+		cliente.setNombre(resultado.getString("nombre"));
+		cliente.setApellidos(resultado.getString("apellidos"));
+		cliente.setDireccion(resultado.getString("direccion"));
+		cliente.setLocalidad(resultado.getString("localidad"));
+		
+		return cliente;
+	}
 }
